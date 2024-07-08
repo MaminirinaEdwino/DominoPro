6731a159c74b5084c028552401d363abba69dc5d
MaminirinaEdwino <MaminirinaEdwino@users.noreply.github.com>
2024-07-08 17:11:39 +0000

    deploy: aa5b13e3cb478dabb4a481f6a2ff65bacad3c4aa

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/DominoPro/sw.js', { scope: '/DominoPro/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
